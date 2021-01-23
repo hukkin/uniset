0cb5b434fd758793bd8900d1b3fe05d8a82393a3
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-01-23 17:36:49 +0200

    Fix test

--- tests/test_maincategories.py
+++ tests/test_maincategories.py
@@ -1,9 +1,7 @@
 import uniset
 
 
-def test_c():
-    assert "\t" in uniset.C  # From Cc subcategory
-    assert "\u0603" in uniset.C  # From Cf subcategory
-    assert "\U000c8550" in uniset.C  # From Cn subcategory
-    assert "\ue855" in uniset.C  # From Co subcategory
-    assert "\ude34" in uniset.C  # From Cs subcategory
+def test_m():
+    assert "ᰫ" in uniset.M  # From Mc subcategory
+    assert "⃤" in uniset.M  # From Me subcategory
+    assert "ၳ" in uniset.M  # From Mn subcategory
