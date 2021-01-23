4303a29e180a641bd672f6c0bbc75e3f7b61e136
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-01-24 01:43:45 +0200

    Bump version: 0.0.0 → 0.0.1

--- uniset/__init__.py
+++ uniset/__init__.py
@@ -1,7 +1,7 @@
 from uniset._category import MAINCATEGORIES, SUBCATEGORIES
 
 __all__ = SUBCATEGORIES + MAINCATEGORIES + ("WHITESPACE", "PUNCTUATION")
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 import importlib
 import string
